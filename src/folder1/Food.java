3bd16af351551bcd38fceb696f6f2b71e39cd886
IlyaK <ilyakulidi@gmail.com>
2022-08-30 22:30:27 -0400

    Created Food.java file with 2 lines

---
+++ src/folder1/Food.java
@@ -0,0 +1,10 @@
+package folder1;
+
+public class Food {
+    public static void main(String[] args) {
+
+        // do not eat after 10 PM
+
+        // do not drink Alcoholic drinks before sleep
+    }
+}
